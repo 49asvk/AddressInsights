7ef460a94f74550e11ac52d13a281c044df18413
49asvk <49asvk@gmail.com>
2026-09-02 02:02:49 +0530

    vite configuration changed

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,6 @@ export default defineConfig({
   base: "/AddressInsights/",
   server: {
     port: 5173,
-    allowedHosts: ['der-presenting-sunrise-queens.trycloudflare.com', 'addressinsights.debarv.com'],
+    allowedHosts: ['https://49asvk.github.io/AddressInsights/', '49asvk.github.io', '49asvk.github.io/AddressInsights' ],
   },
 });
