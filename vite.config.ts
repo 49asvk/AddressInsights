5f0899dc026f683bf4d9faac5fe1b9d45e6af4b4
49asvk <49asvk@gmail.com>
2026-09-02 01:15:17 +0530

    deployment configuration

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from "vite";
 
 export default defineConfig({
+  base: "/AddressInsights/",
   server: {
     port: 5173,
     allowedHosts: ['der-presenting-sunrise-queens.trycloudflare.com', 'addressinsights.debarv.com'],
