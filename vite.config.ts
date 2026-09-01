b660c5b76f83ed7594c771914261cbd568bc8071
49asvk <49asvk@gmail.com>
2026-09-02 02:27:02 +0530

    vite conf changed

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,6 @@ export default defineConfig({
   base: "/AddressInsights/",
   server: {
     port: 5173,
-    allowedHosts: ['https://49asvk.github.io/AddressInsights/', '49asvk.github.io', '49asvk.github.io/AddressInsights' ],
+    allowedHosts: ['https://49asvk.github.io/AddressInsights/', '49asvk.github.io', '49asvk.github.io/AddressInsights', 'https://49asvk.github.io/*'],
   },
 });
